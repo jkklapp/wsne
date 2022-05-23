e10b49f9644a95878ccf138a45fa205bbc164c02
Jaakko Lappalainen <jkk.lapp@gmail.com>
2022-05-23 11:06:13 +0100

    chore: setup firebase

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import * as functions from 'firebase-functions';
+import * as express from 'express';
+
+const server = express();
+
+const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  app.enableCors();
+  return app.init();
+};
+
+createNestServer(server)
+  .then((v) => console.log('Nest ready'))
+  .catch((err) => console.log('Nest broken', err));
+
+export const api = functions.https.onRequest(server);

4fa3ddfc8fab29f8e66dff04db832862e4f814b6
Jaakko Lappalainen <jkk.lapp@gmail.com>
2022-05-24 17:48:04 +0100

    feat: add guards for posts service

--- backend/src/functions/posts/controller.ts
+++ backend/src/functions/posts/controller.ts
@@ -1,16 +1,26 @@
-import { Body, Controller as BaseController, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller as BaseController,
+  Get,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { PostDocument } from './document';
 import { Service } from './service';
+import { FirebaseAuthGuard } from '../firebase/firebase-auth.guard';
 
 @BaseController('posts')
 export class Controller {
   constructor(private readonly service: Service) {}
 
   @Get()
+  @UseGuards(FirebaseAuthGuard)
   findAll(): Promise<PostDocument[]> {
+    console.log('findAll');
     return this.service.findAll();
   }
   @Post()
+  @UseGuards(FirebaseAuthGuard)
   public create(@Body() post: PostDocument): Promise<PostDocument> {
     return this.service.create(post);
   }
--- backend/src/test/app.e2e-spec.ts
+++ backend/src/test/app.e2e-spec.ts
@@ -2,20 +2,39 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../functions/app.module';
+import { FirebaseAuthGuard } from '../functions/firebase/firebase-auth.guard';
+import { CanActivate } from '@nestjs/common';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      // .overrideGuard(FirebaseAuthGuard)
+      // .useValue({ canActivate: jest.fn(() => true) })
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('returns an empty list of posts', () => {
-    return request(app.getHttpServer()).get('/posts').expect(200).expect([]);
+  describe('/posts', () => {
+    describe('when doing a GET /posts unauthorized', () => {
+      it('should return 401', () => {
+        return request(app.getHttpServer()).get('/posts').expect(401);
+      });
+    });
+    xdescribe('when doing a GET /posts authorized', () => {
+      it('should return a 200 with an empty list', () => {
+        return request(app.getHttpServer())
+          .get('/posts')
+          .set('Accept', 'application/json')
+          .set('Authorization', 'Bearer token')
+          .expect(200)
+          .expect([]);
+      });
+    });
   });
 });

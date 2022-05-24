c9183a589469c51b56e42ae426789883cc37aa11
Jaakko Lappalainen <jkk.lapp@gmail.com>
2022-05-24 12:56:37 +0100

    feat: added vue and login with firebase

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import App from './App.vue';
+import store from './store';
+import router from './routes/index';
 
-createApp(App).mount('#app')
+const firebaseConfig = {
+  apiKey: 'AIzaSyDzBNz_bMRHUTxdrhc4LCf4UMzTIyyRB5s',
+  authDomain: 'wsne-28b5b.firebaseapp.com',
+  projectId: 'wsne-28b5b',
+  storageBucket: 'wsne-28b5b.appspot.com',
+  messagingSenderId: '612021884642',
+  appId: '1:612021884642:web:e1a932d5b80c56b46770bb',
+  measurementId: 'G-J8576H162E',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged((user) => {
+  store.dispatch('fetchUser', user);
+});
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.mount('#app');

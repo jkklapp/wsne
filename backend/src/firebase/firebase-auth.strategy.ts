57dca06250db19845a2160a8cc9ad67d4bc4d0a1
Jaakko Lappalainen <jkk.lapp@gmail.com>
2022-05-26 09:06:41 +0100

    refactor: avoid using forbidden prefix for env vars

--- backend/src/firebase/firebase-auth.strategy.ts
+++ backend/src/firebase/firebase-auth.strategy.ts
@@ -16,8 +16,8 @@ export class FirebaseAuthStrategy extends PassportStrategy(
     const firebase_params = {
       type: process.env.FB_PARAMS_TYPE,
       projectId: process.env.FB_PARAMS_PROJECT_ID,
-      privateKeyId: process.env.FIREBASE_PARAMS_PRIVATE_KEY_ID,
-      privateKey: process.env.FIREBASE_PARAMS_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      privateKeyId: process.env.FB_PARAMS_PRIVATE_KEY_ID,
+      privateKey: process.env.FB_PARAMS_PRIVATE_KEY.replace(/\\n/g, '\n'),
       clientEmail: process.env.FB_PARAMS_CLIENT_EMAIL,
       clientId: process.env.FB_PARAMS_CLIENT_ID,
       authUri: process.env.FB_PARAMS_AUTH_URI,

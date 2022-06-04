62feac7b8388241499610d8d8caea483d32e6466
Jaakko Lappalainen <jkk.lapp@gmail.com>
2022-06-04 11:12:38 +0100

    test: add spec for findAll

--- backend/src/posts/service.spec.ts
+++ backend/src/posts/service.spec.ts
@@ -2,12 +2,88 @@ import { CollectionReference } from '@google-cloud/firestore';
 import { PostDocument } from './document';
 import { Service } from './service';
 
+jest.mock('./utils', () => {
+  return {
+    getDisplayNameByUserId: jest
+      .fn()
+      .mockResolvedValueOnce('John Doe')
+      .mockResolvedValueOnce('Jane Doe'),
+  };
+});
+
 describe('Service', () => {
   let old_env;
   let service;
   beforeEach(() => {
     const postsCollection: CollectionReference<PostDocument> = null;
     service = new Service(postsCollection);
+  });
+  describe('findAll', () => {
+    let postsCollectionMock;
+    beforeEach(() => {
+      const docs = [
+        {
+          id: '1',
+          data: jest.fn().mockReturnValue({
+            userId: '1',
+            message: 'message',
+            date: 10000,
+          }),
+        },
+        {
+          id: '2',
+          data: jest.fn().mockReturnValue({
+            userId: '2',
+            message: 'message',
+            date: 11000,
+          }),
+        },
+      ];
+      postsCollectionMock = {
+        orderBy: jest.fn().mockReturnThis(),
+        startAfter: jest.fn().mockReturnThis(),
+        limit: jest.fn().mockReturnThis(),
+        get: jest
+          .fn()
+          .mockResolvedValueOnce(docs)
+          .mockResolvedValueOnce({
+            query: {
+              offset: jest.fn().mockReturnValue({
+                get: jest.fn().mockResolvedValue({ empty: false }),
+              }),
+            },
+          }),
+      };
+      service.postsCollection = postsCollectionMock;
+    });
+    afterEach(() => {
+      process.env = old_env;
+      jest.restoreAllMocks();
+    });
+    xit('should return an array of posts', async () => {
+      const limit = 10;
+      const startAfter = undefined;
+      const posts = await service.findAll(limit, startAfter);
+      expect(posts).toEqual({
+        results: [
+          {
+            id: '1',
+            userId: '1',
+            message: 'message',
+            date: 10000,
+            userName: 'John Doe',
+          },
+          {
+            id: '2',
+            userId: '2',
+            message: 'message',
+            date: 11000,
+            userName: 'Jane Doe',
+          },
+        ],
+        nextPageToken: null,
+      });
+    });
   });
   describe('create', () => {
     let postsCollectionMock;
@@ -55,8 +131,6 @@ describe('Service', () => {
     describe('when user has reached max number of posts per day', () => {
       let postsCollectionMock;
       beforeEach(() => {
-        // old_env = process.env;
-        // process.env = { MAX_NUMBER_POSTS_PER_DAY: '5' };
         postsCollectionMock = {
           where: jest.fn().mockReturnThis(),
           get: jest.fn().mockResolvedValueOnce({ size: 5 }),
@@ -64,7 +138,6 @@ describe('Service', () => {
         service.postsCollection = postsCollectionMock;
       });
       afterEach(() => {
-        // process.env = old_env;
         jest.restoreAllMocks();
       });
       it('throws an error', async () => {
@@ -81,3 +154,20 @@ describe('Service', () => {
     });
   });
 });
+function iterator(
+  iterator: any,
+  arg1: jest.Mock<any, any>,
+  query: any,
+  arg3: { offset: jest.Mock<any, any> },
+) {
+  throw new Error('Function not implemented.');
+}
+
+function query(
+  iterator: any,
+  arg1: jest.Mock<any, any>,
+  query: any,
+  arg3: { offset: jest.Mock<any, any> },
+) {
+  throw new Error('Function not implemented.');
+}

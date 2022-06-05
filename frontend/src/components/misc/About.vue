3143cdd73c511c6d8a84d7b58b7a0df9ce4590a0
Jaakko Lappalainen <jkk.lapp@gmail.com>
2022-06-05 17:52:17 +0100

    refactor(About): improved styling

--- frontend/src/components/misc/About.vue
+++ frontend/src/components/misc/About.vue
@@ -1,26 +1,33 @@
 <template>
   <div
-    class="h-screen row justify-content-center bg-white border-gray-200 dark:bg-gray-600"
+    class="mx-auto px-24 pt-10 pb-8 h-screen row justify-content-center bg-white border-gray-200 dark:bg-gray-600 text-gray-700 dark:text-gray-100"
   >
-    <h1>About</h1>
+    <h1 class="text-6xl text-center mt-12 mb-14 md:my-2">About</h1>
+    <br />
+    <br />
     <p>
       {{ appName }} is a micro-blogging site with a focus on simplicity and
       clean design.
     </p>
+    <br />
     <p>
-      I created {{ appName }} as a side project to learn Firebase, NestJS and
-      VueJS.
+      I created {{ appName }} as a side project to learn Firebase and NestJS in
+      the backend, and VueJS and VueX in the frontend. I also use TailwindCSS
+      for styling.
     </p>
+    <br />
     <p>
       The terms and conditions and privacy policy have been adapted from
       Twitter's. Please contact send me an email to
       <a href="mailto:{{ supportEmail }}">{{ supportEmail }}</a> if you have any
       questions or concerns. I will do my best to address them.
     </p>
+    <br />
     <p>
       The source code that powers this site is on
       <a href="https://github.com/jkklapp/wsne">my github</a> page.
     </p>
+    <br />
   </div>
 </template>
 <script>

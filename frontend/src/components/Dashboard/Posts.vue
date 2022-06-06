de608bc3c2a6849aa4d4e0487436428764842646
Jaakko Lappalainen <jkk.lapp@gmail.com>
2022-06-06 09:24:28 +0100

    refactor(Posts): improve styling in mobile

--- frontend/src/components/Dashboard/Posts.vue
+++ frontend/src/components/Dashboard/Posts.vue
@@ -4,13 +4,16 @@
       <div
         v-for="p in posts"
         :key="p.id"
-        class="bg-gray-200 dark:bg-gray-500 text-gray-800 dark:text-gray-100 px-4 pt-2 pb-2 mb-2 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg"
+        class="bg-gray-200 dark:bg-gray-500 text-gray-800 dark:text-gray-100 px-4 pt-2 pb-2 mb-2 shadow-xl ring-1 ring-gray-900/5 max-w-sm mx-2 md:max-w-lg sm:mx-auto sm:rounded-lg"
       >
         <div class="flex flex-wrap">
           <div>
             <small>{{ p.userName }} - {{ date(p.date) }}</small>
           </div>
-          <div v-if="!p.id && isPosting" class="w-100 ml-auto place-items-end">
+          <div
+            v-if="!p.id && isPosting"
+            class="w-90 md:w-100 place-items-center"
+          >
             <div class="lds-ring-small align-text-top">
               <div></div>
               <div></div>

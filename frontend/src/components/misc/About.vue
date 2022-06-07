9e760017a2884ffd947269ff5929f492192330a2
Jaakko Lappalainen <jkk.lapp@gmail.com>
2022-06-07 15:21:27 +0100

    fix(About): improve styling and content

--- frontend/src/components/misc/About.vue
+++ frontend/src/components/misc/About.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="grid md:place-items-center">
     <div>
-      <h1 class="text-6xl text-center mt-12 mb-4 md:my-2">
+      <h1 class="text-6xl text-center mt-12 md:mt-32 mb-4 md:mb-2">
         <span class="text-gray-700 dark:text-gray-100">
           <span class="font-bold">About</span>
         </span>
       </h1>
       <div
-        class="dark:text-white bg-gray-200 dark:bg-gray-500 px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 md:rounded-lg"
+        class="dark:text-white bg-gray-200 dark:bg-gray-500 px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 md:rounded-lg md:w-1/2 mx-auto"
       >
-        <br />
-        <br />
         <p>
           What if you could only <strong>share 10 ideas a day</strong>? Would
           you start typing or rather
@@ -23,21 +21,41 @@
         </p>
         <br />
         <p>
-          I created {{ appName }} as a side project to learn Firebase and NestJS
-          in the backend, and VueJS and VueX in the frontend. I also use
-          TailwindCSS for styling.
+          I created {{ appName }} as a side project to learn
+          <strong>Firebase</strong> and <strong>NestJS</strong> in the backend,
+          and <strong>VueJS</strong> and <strong>VueX</strong> in the frontend.
+          I also use <strong>TailwindCSS</strong> for styling.
         </p>
         <br />
         <p>
-          The terms and conditions and privacy policy have been adapted from
-          Twitter's. Please contact send me an email to
-          <a href="mailto:{{ supportEmail }}">{{ supportEmail }}</a> if you have
-          any questions or concerns. I will do my best to address them.
+          The source code that powers this site is available on
+          <a
+            target="_blank"
+            class="text-blue-600 dark:text-blue-400"
+            href="https://github.com/jkklapp/wsne"
+            >github</a
+          >.
         </p>
         <br />
         <p>
-          The source code that powers this site is on
-          <a href="https://github.com/jkklapp/wsne">my github</a> page.
+          The
+          <router-link class="text-blue-600 dark:text-blue-400" to="terms"
+            >terms and conditions</router-link
+          >
+          and
+          <router-link
+            class="text-blue-600 dark:text-blue-400"
+            to="privacy-policy"
+            >privacy policy</router-link
+          >
+          have been adapted from Twitter's. Please send me an email to
+          <a
+            class="text-blue-600 dark:text-blue-400"
+            href="mailto:{{ supportEmail }}"
+            >{{ supportEmail }}</a
+          >
+          if you have any questions or concerns. I will do my best to address
+          them.
         </p>
         <br />
       </div>

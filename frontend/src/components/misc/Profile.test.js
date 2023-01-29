db6220de2877e6ec93d2fee5914cd5ca24d33761
Jaakko Lappalainen <jkk.lapp@gmail.com>
2023-01-29 12:21:09 +0000

    add empty profile page

---
+++ frontend/src/components/misc/Profile.test.js
@@ -0,0 +1,35 @@
+import { render } from '@testing-library/vue';
+import Profile from './Profile.vue';
+import { store } from '../../store';
+import { createStore } from 'vuex';
+
+const renderComponent = (customStore) => {
+  const mergedStoreInstance = createStore({
+    ...store,
+    ...customStore,
+  });
+  return render(Profile, {
+    global: {
+      plugins: [mergedStoreInstance],
+    },
+  });
+};
+
+describe('Profile', () => {
+  describe('when user is not yet loaded', () => {
+    test('it shows a question mark', async () => {
+      const component = renderComponent({
+        state: {
+          user: {
+            email: '',
+            displayName: '',
+          },
+        },
+      });
+
+      const { getByText } = component;
+
+      getByText('Name: ?');
+    });
+  });
+});

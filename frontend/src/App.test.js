98d388a61606a9db8c01c2d69ee280756cf4ebbd
Jaakko Lappalainen <jkk.lapp@gmail.com>
2023-01-29 18:09:08 +0000

    add email to profile (#115)
    * add email to profile
    
    * add instructions to start firebase locally
    
    * add tests case for loading empty posts list
    
    * remove console.log
    
    * cleanup test
    
    * added test for profile

---
+++ frontend/src/App.test.js
@@ -0,0 +1,90 @@
+/* eslint-disable vue/require-prop-types */
+/* eslint-disable vue/one-component-per-file */
+
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/vue';
+import App from './App';
+import { store } from './store';
+import router from './router';
+import { getAuth } from './auth';
+import { createStore } from 'vuex';
+import { apiRequest } from './store/actions/api';
+
+jest.mock('./auth');
+jest.mock('./store/actions/api');
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+const userFixture = {
+  email: 'test',
+  displayName: 'John Doe',
+  emailVerified: true,
+};
+
+describe('when logged in', () => {
+  beforeEach(() => {
+    getAuth.mockReturnValue({
+      onAuthStateChanged: jest.fn(() => userFixture),
+    });
+  });
+  describe('when no posts exist', () => {
+    let component;
+    beforeEach(() => {
+      apiRequest.mockResolvedValueOnce({
+        data: {
+          results: [],
+          remainingMessages: 1,
+          nextPageToken: null,
+        },
+      });
+      const initialState = {
+        ...store,
+        state: {
+          ...store.state,
+          user: userFixture,
+          loggedIn: true,
+        },
+      };
+
+      const storeInstance = createStore(initialState);
+      component = render(App, {
+        global: {
+          plugins: [storeInstance, router],
+        },
+      });
+    });
+    describe('when navigating to /', () => {
+      test('it renders "No posts yet"', async () => {
+        router.push('/');
+        await router.isReady();
+
+        const { findByText } = component;
+
+        await findByText('No posts yet.');
+      });
+    });
+    describe('when navigating to /profile', () => {
+      test('it shows name and email', async () => {
+        router.push('/profile');
+        await router.isReady();
+
+        const { findByText } = component;
+
+        await findByText('Name: John Doe');
+        await findByText('Email: test');
+      });
+    });
+  });
+});
